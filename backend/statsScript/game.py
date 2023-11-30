f6c1ef3a3983d5d564acaa1fe4eae903cfd6c0b7
rxfa <rafa3rnrn@gmail.com>
2023-11-30 22:18:17 +0000

    Got rid of useless functions and wrote exception that stops script from running if the first round does not have a full lobby

--- backend/statsScript/game.py
+++ backend/statsScript/game.py
@@ -1,14 +1,14 @@
 from collections import namedtuple
-from tabulate import tabulate
-from utils import is_own_team, is_opp_team, get_side, ID, print3n
-from player import Player
 from round import Round
 
 Score = namedtuple("Score", "own opp")
 
 
-class Match:
+class Game:
     def __init__(self, data):
+        if len(data["rounds"][0]["players"]) != 10:
+            raise Exception("Game must be started with a full lobby(10 players)!")
+
         self.map: str = data["rounds"][0]["map"]["name"]
         self.score: Score = Score(
             own=data["rounds"][-1]["teams"][0]["score"],
--- backend/statsScript/utils.py
+++ backend/statsScript/utils.py
@@ -1,21 +1,5 @@
 import functools
 import time
-from enum import Enum
-
-
-class ID(Enum):
-    KILL_ID = 0
-    DEATH_ID = 1
-    DEFUSER_PLANT_START_ID = 2
-    DEFUSER_PLANTED_ID = 3
-    DEFUSER_DISABLED_ID = -1  # TODO: Find out the ID
-    LOCATE_OBJECTIVE_ID = 6
-    OPERATOR_SWAP_ID = 7
-    OTHER_ID = 10
-
-
-def print3n(arg):
-    print(arg, end="\n\n\n")
 
 
 def timer(func):
@@ -29,18 +13,3 @@ def timer(func):
         return value
 
     return wrapper_timer
-
-
-def is_own_team(team, team_name="YOUR TEAM"):
-    return True if team["name"] == team_name else False
-
-
-def is_opp_team(team, team_name="OPPONENTS"):
-    return True if team["name"] == team_name else False
-
-
-def get_side(src, number=0):
-    try:
-        return list(filter(is_own_team, src["rounds"][number]["teams"]))[0]["role"]
-    except IndexError as err:
-        print("Runtime error:", err)
