c38a84352dd55c84bcfc717db192806cb4e1d16c
rxfa <rafa3rnrn@gmail.com>
2024-04-30 21:31:58 +0100

    Vods can now be created and deleted from frontend

--- frontend/src/components/AddVOD.js
+++ frontend/src/components/AddVOD.js
@@ -8,9 +8,29 @@ import {
     useDisclosure
 } from "@chakra-ui/react";
 import {AddIcon, CloseIcon} from "@chakra-ui/icons";
+import React, { useState } from 'react';
+import { addVod } from "@/app/api/vods";
 
 export default function AddVOD(){
     const { isOpen, onOpen, onClose } = useDisclosure()
+
+    const [against, setAgainst] = useState('')
+    const [notes, setNotes] = useState('')
+    const [url, setUrl] = useState('')
+
+    const handleClick = event => {
+        event.preventDefault()
+        const form = {
+            "against": against, 
+            "notes": notes, 
+            "url": url
+        }
+        addVod(form)
+        .then(() => {
+            onClose()
+        })
+    }
+
     return(
         <>
             <Button onClick={onOpen} w={"100%"}>Add VOD</Button>
@@ -20,44 +40,46 @@ export default function AddVOD(){
                 <ModalContent>
                     <ModalHeader>Add VOD</ModalHeader>
                     <ModalCloseButton />
-                    <ModalBody>
-                        <Stack gap={4}>
-                            <FormControl isRequired>
-                                <FormLabel>Vs</FormLabel>
-                                <Input type='text' />
-                                <FormHelperText>The team you played against</FormHelperText>
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel>Notes</FormLabel>
-                                <Input type='text' />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel>VOD URL</FormLabel>
-                                <InputGroup size='sm'>
-                                    <InputLeftAddon>
-                                        https://
-                                    </InputLeftAddon>
-                                    <Input placeholder='vod link' />
-                                    <InputRightAddon>
-                                        .com
-                                    </InputRightAddon>
-                                </InputGroup>
-                                <FormHelperText>The link to your vod. Might be youtube, twitch, etc...</FormHelperText>
-                            </FormControl>
-                        </Stack>
-                    </ModalBody>
+                    <form onSubmit={handleClick}>
+                        <ModalBody>
+                            <Stack gap={4}>
+                                <FormControl isRequired>
+                                    <FormLabel>Vs</FormLabel>
+                                    <Input type='text' onChange={e => setAgainst(e.currentTarget.value)}/>
+                                    <FormHelperText>The team you played against</FormHelperText>
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel>Notes</FormLabel>
+                                    <Input type='text' onChange={e => setNotes(e.currentTarget.value)}/>
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel>VOD URL</FormLabel>
+                                    <InputGroup size='sm'>
+                                        <InputLeftAddon>
+                                            https://
+                                        </InputLeftAddon>
+                                        <Input placeholder='vod link' onChange={e => setUrl(e.currentTarget.value)}/>
+                                        <InputRightAddon>
+                                            .com
+                                        </InputRightAddon>
+                                    </InputGroup>
+                                    <FormHelperText>The link to your vod. Might be youtube, twitch, etc...</FormHelperText>
+                                </FormControl>
+                            </Stack>
+                        </ModalBody>
 
-                    <ModalFooter mt={4}>
-                        <Button
-                            colorScheme='blue'
-                            mr={3}
-                            onClick={onClose}
-                            rightIcon={<CloseIcon/>}
-                        >
-                            Close
-                        </Button>
-                        <Button rightIcon={<AddIcon/>}>Add</Button>
-                    </ModalFooter>
+                        <ModalFooter mt={4}>
+                            <Button
+                                colorScheme='blue'
+                                mr={3}
+                                onClick={onClose}
+                                rightIcon={<CloseIcon/>}
+                            >
+                                Close
+                            </Button>
+                            <Button rightIcon={<AddIcon/>} type="submit">Add</Button>
+                        </ModalFooter>
+                    </form>
                 </ModalContent>
             </Modal>
         </>
